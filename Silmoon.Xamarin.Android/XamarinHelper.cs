6822acea2769a034dce1dad2ae940b00135441e0
WEIBIN SONG <sone@silmoon.com>
2023-03-09 23:33:36 +0800

    有的时候显式注册服务是必要的。。。

---
+++ Silmoon.Xamarin.Android/XamarinHelper.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Silmoon.Xamarin.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Silmoon.Xamarin.Android
+{
+    public class XamarinHelper
+    {
+        public static void RegisterServices()
+        {
+            DependencyService.Register<IFileService, FileService>();
+            DependencyService.Register<IAppInstaller, AppInstaller>();
+            //DependencyService.Register<INotificationManager, AndroidNotificationManager>();
+            DependencyService.Register<IAppPackage, AppPackage>();
+        }
+    }
+}
---
+++ Silmoon.Xamarin.iOS/XamarinHelper.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using Silmoon.Xamarin.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+
+namespace Silmoon.Xamarin.iOS
+{
+    public class XamarinHelper
+    {
+        public static void RegisterServices()
+        {
+            DependencyService.Register<IFileService, FileService>();
+            DependencyService.Register<IInAppPurchase, InAppPurchase>();
+            DependencyService.Register<INotificationManager, iOSNotificationManager>();
+            DependencyService.Register<IAppPackage, AppPackage>();
+        }
+    }
+}
