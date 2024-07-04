52d3e15ff04e3500c0665c332cedf179e21a7dde
WEIBIN SONG <sone@silmoon.com>
2024-07-04 08:27:18 +0800

    增加一个IOSShellTabbarIconScaleRenderer，用于处理Shell的Tabbar Icon在特殊的分辨率下尺寸过大的问题。

---
+++ Silmoon.Maui/Platforms/iOS/Handlers/IOSShellTabbarIconScaleRenderer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreGraphics;
+using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Controls.Platform.Compatibility;
+using UIKit;
+
+namespace Silmoon.Maui.Handlers
+{
+    public class IOSShellTabbarIconScaleRenderer : ShellRenderer
+    {
+        protected override IShellTabBarAppearanceTracker CreateTabBarAppearanceTracker()
+        {
+            return new IOSShellTabBarAppearanceTabbarIconScaleTracker();
+        }
+    }
+    public class IOSShellTabBarAppearanceTabbarIconScaleTracker : IShellTabBarAppearanceTracker
+    {
+        public void Dispose()
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void ResetAppearance(UITabBarController controller)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void SetAppearance(UITabBarController controller, ShellAppearance appearance)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void UpdateLayout(UITabBarController controller)
+        {
+            foreach (var tabbarItem in controller.TabBar.Items)
+            {
+                tabbarItem.Image = tabbarItem.Image?.Scale(new CGSize(24, 24));
+            }
+        }
+    }
+}
