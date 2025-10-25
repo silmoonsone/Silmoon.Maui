c154313d0ddfc5e23a3d6f86eb1bc3d7337e11a3
WEIBIN SONG <sone@silmoon.com>
2025-10-26 06:01:32 +0800

    新增StreamToImageSourceConvert转换器。

---
+++ Silmoon.Maui/Converters/StreamToImageSourceConvert.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silmoon.Maui.Converters
+{
+    public class StreamToImageSourceConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return ByteToImageSource(value as Stream);
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+        public static ImageSource ByteToImageSource(Stream stream) => stream == null || stream.Length == 0 ? null : ImageSource.FromStream(() => stream);
+    }
+}
