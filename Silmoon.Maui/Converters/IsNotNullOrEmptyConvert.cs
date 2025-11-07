89a4d997028716e85edd5293b911303f290bd345
WEIBIN SONG <sone@silmoon.com>
2025-11-08 01:25:24 +0800

    add IsNotNullOrEmptyConvert convert.

---
+++ Silmoon.Maui/Converters/IsNotNullOrEmptyConvert.cs
@@ -0,0 +1,30 @@
+﻿using Silmoon.Extension;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Silmoon.Maui.Converters
+{
+    public class IsNotNullOrEmptyConvert : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return false;
+            else if (value is string str)
+                return !str.IsNullOrEmpty();
+            else if (value is ICollection collection && collection.Count > 1)
+                return true;
+            else if (value is Array array && array.Length > 1)
+                return true;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
