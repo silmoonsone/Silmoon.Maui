fe3944148421ab375a053c2239f57568dd2bba23
WEIBIN SONG <sone@silmoon.com>
2025-11-17 09:45:17 +0800

    新增ContentPageExtensions扩展方法类型，使用C#14 extension语法。

---
+++ Silmoon.Maui/Extensions/ContentPageExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Silmoon.Maui.Extensions
+{
+    public static class ContentPageExtension
+    {
+        extension(ContentPage page)
+        {
+            public bool IsModalPage()
+            {
+                if (page.Navigation is not null)
+                    return page.Navigation.ModalStack.Contains(page);
+                else return false;
+            }
+            public async Task<bool> PopAsync()
+            {
+                if (page.Navigation is not null)
+                {
+                    if (page.IsModalPage())
+                        await page.Navigation.PopModalAsync();
+                    else
+                        await page.Navigation.PopAsync();
+                    return true;
+                }
+                else return false;
+            }
+        }
+    }
+}
